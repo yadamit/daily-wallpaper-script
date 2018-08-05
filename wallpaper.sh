6798d897b8e0cd25de87ecf2f16ff1f707fdac47
Amit yadav <yamit@iitk.ac.in>
2018-08-05 19:44:49 +0530

    initial push

---
+++ wallpaper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~/Downloads/nasa_daily_photo
+rm *.jpg *.txt index.html
+wget http://www.backgroundimageshd.com/gallery/full-hd-1080p-nature-wallpapers/
+grep 'img class="wp' index.html > tmp1.txt
+awk -F "\"" '{print $18}' tmp1.txt > tmp2.txt
+rm tmp1.txt
+N=$(wc -l tmp2.txt)
+N=$(echo $N | awk '{print $1}' )
+rand=$(shuf -i 1-$N -n 1)
+a=$(awk "FNR==$rand" tmp2.txt )
+wget $a
+imname=$(ls | grep *.jpg)
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
+
+

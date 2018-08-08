a972b46cb5023c3bd17734610d03ed23397b823d
Amit yadav <yamit@iitk.ac.in>
2018-08-08 12:08:12 +0530

    Added new files

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/Downloads/nasa_daily_photo
+cd ~/Downloads/daily-wallpaper-script
 rm *.jpg *.txt index.html
 wget http://www.backgroundimageshd.com/gallery/full-hd-1080p-nature-wallpapers/
 grep 'img class="wp' index.html > tmp1.txt
@@ -9,6 +9,7 @@ rm tmp1.txt
 N=$(wc -l tmp2.txt)
 N=$(echo $N | awk '{print $1}' )
 rand=$(shuf -i 1-$N -n 1)
+echo $rand > rand.txt
 a=$(awk "FNR==$rand" tmp2.txt )
 wget $a
 imname=$(ls | grep *.jpg)
